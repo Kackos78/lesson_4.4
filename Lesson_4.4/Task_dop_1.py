efe93639a0018eb2b14b2bd03ee6a3fb8347878a
Daniil <oleinik.d_78@mail.ru>
2023-09-01 21:38:35 +0300

    Task_dop_1 done

--- Lesson_4.4/Task_dop_1.py
+++ Lesson_4.4/Task_dop_1.py
@@ -4,4 +4,41 @@
 # например, k=2 -> -x^2 + 3*x - 8 = 0
 # тут коэффициенты -1,3,-8
 # например, k=3 -> 3*x^3 - 2*x = 0
-# тут коэффициенты 3,0,-2,0
+# тут коэффициенты 3, 0, -2, 0
+from random import randint
+def input_k():
+    k = int(input("Введите какое-то натуральное число: "))
+    return k
+def make_coefficient(k)-> list:
+    sp = []
+    i = k
+    while i != -1:
+        sp.append(randint(-10, 10))
+        i -= 1
+    print(sp)
+    return sp
+
+def equation(k, sp)-> list:
+    list = []
+    i = 0
+    count = k
+    while count != 0:
+        if sp[i] == 0:
+            i += 1 
+            count -= 1
+        if sp[i] > 0 and len(list) != 0:
+            list.append(f" +{sp[i]} x^{count}")
+        elif count != 1:
+            list.append(f" {sp[i]} x^{count}")
+        else:
+            list.append(f" {sp[i]} x")
+        i += 1 
+        count -= 1
+    list.append(" = 0")
+    return list
+
+def printing_equation(list) -> print:
+    print("".join(list)) 
+k = input_k()
+list = make_coefficient(k)
+printing_equation(equation(k, list))
