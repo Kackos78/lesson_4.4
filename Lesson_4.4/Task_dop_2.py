8cbe56601ad9492237e08ebcae9114edce4a9e19
Daniil <oleinik.d_78@mail.ru>
2023-09-02 13:48:24 +0300

    Task_dop_2 done

--- Lesson_4.4/Task_dop_2.py
+++ Lesson_4.4/Task_dop_2.py
@@ -5,3 +5,29 @@
 # Избегайте магических чисел
 # Добавьте аннотацию типов где это возможно
 # Используйте функции
+
+def input_num()->int: 
+    num = int(input("Введите целое число: ")) 
+    return num
+
+def make_bin(num: int)-> int:
+    ansver = ''
+    while num != 0:
+        ansver = ansver + str(num % 2)
+        num = num // 2
+    ansver = ''.join(reversed(ansver))
+    return '0b' + ansver
+
+def make_oct(num: int)->int:
+    if num == 0:
+        return '0o'
+    else:
+        res = str(num % 8)
+        return str(make_oct(num // 8)) + res
+    
+num = input_num()
+
+print(make_bin(num))
+print(make_oct(num))
+print(bin(num))
+print(oct(num))
