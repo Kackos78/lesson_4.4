228e55de170f8625890dc9c54ac37b85c1f8a863
Daniil <oleinik.d_78@mail.ru>
2023-09-09 20:36:47 +0300

    Lesson_7.7

--- Lesson_7/Task_47.py
+++ Lesson_7/Task_47.py
@@ -22,7 +22,7 @@
 
 transformation = lambda values: values
 values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] 
-transormed_values = list(map( transformation, values))
+transormed_values = list(map(transformation, values))
 if values == transormed_values:
     print('ok')
 else:
